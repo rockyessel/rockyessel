a290453431fa9689b741279d674cff0aba7e3b41
Rocky Essel <rockyessel76@gmail.com>
2023-02-01 21:59:22 +0000

    added a loaded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Navbar, Footer } from '@/components';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Navbar />
+      <NextNProgress color={'#d1d5db'} />
       <Component {...pageProps} />
       <Footer />
     </React.Fragment>
--- utils/nodemailer.ts
+++ utils/nodemailer.ts
@@ -1,6 +1,5 @@
 import nodemailer from 'nodemailer';
 
-const { NEXT_PUBLIC_EMAIL, NEXT_PUBLIC_EMAIL_PASS } = process.env;
 
 const email = process.env.NEXT_PUBLIC_EMAIL;
 const pass = process.env.NEXT_PUBLIC_EMAIL_PASS;
