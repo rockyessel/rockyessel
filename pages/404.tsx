7892a5af977de263dd2f85fa94d691a4398e261f
Rocky Essel <rockyessel76@gmail.com>
2023-03-10 23:38:25 +0000

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Layout } from '@/components';
+import Link from 'next/link';
 
 const NotFound = () => {
   const router = useRouter().asPath;
@@ -9,7 +10,7 @@ const NotFound = () => {
     <Layout
       description={'The page you are looking for was not found.'}
       title={'Page Not Found'}
-      image={''}
+      image={'images/notfoundpage.PNG'}
       type={'Website'}
       alt={'Page Not Found'}
       keywords={'Page Not Found'}
@@ -18,8 +19,27 @@ const NotFound = () => {
       MIME={'png'}
       author_name={'Rocky Essel'}
     >
-      <main className='w-full h-full flex justify-center items-center'>
-        <p className='text-7xl '>404</p>
+      <main className='w-full h-[79.2vh] px-4'>
+        <section className=' flex flex-col justify-center mt-5 md:mt-28 items-center gap-3'>
+          <p className='text-7xl '>404</p>
+          <div className='flex-col justify-center items-center gap-2 text-lg prose-xl'>
+            <p>
+              The page you&apos;re requesting has been block or the URL has been
+              changed.
+            </p>
+            <p>
+              If this page result as clicking on an internal of this web
+              application.
+            </p>
+            <p>
+              Kindly contact us, so we can fix this ASAP! essel_r@outlook.com or
+              contact has through the{' '}
+              <Link className='underline' href='/contact'>
+                form
+              </Link>
+            </p>
+          </div>
+        </section>
       </main>
     </Layout>
   );
