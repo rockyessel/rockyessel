81101887c72b026e31ec4cf5199f9a5a9e15fd0e
Rocky Essel <rockyessel76@gmail.com>
2024-10-18 18:17:20 +0000

    (changes): fixed incorrect namespace

--- portfolio/src/app/(seo)/postsitemap.xml/route.ts
+++ portfolio/src/app/(seo)/postsitemap.xml/route.ts
@@ -17,7 +17,6 @@ const generateSitemapXml = async ({ host }: SitemapProps) => {
   return `<?xml version="1.0" encoding="UTF-8"?><urlset
         xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml"
         >
   ${posts
     ?.map((post) => {
@@ -31,8 +30,6 @@ const generateSitemapXml = async ({ host }: SitemapProps) => {
     <lastmod>${post?.updatedAt || generateRandomPreviousDay()}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
-    <meta name="title" content="${post?.title}" />
-    <meta name="description" content="${post?.description}" />
     <image:image>
       <image:loc>${image}</image:loc>
       <image:title>${post?.title}</image:title>
